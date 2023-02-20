1aa62b77029d8f86d052c130bd5e7b675b292444
Pratik Joshi <nutandevjoshi@gmail.com>
2023-02-20 16:04:27 +0530

    first commit

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+import { useTodoListStore } from './../stores/todoList'
+
+const todo = ref('')
+const store = useTodoListStore()
+
+function addItemAndClear(item) {
+  if (item.length === 0) {
+    return
+  }
+  // invokes function in the store:
+  store.addTodo(item)
+  todo.value = ''
+}
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="addItemAndClear(todo)">
+      <input v-model="todo" type="text" /><button>Add</button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/stores/todoList.js
@@ -0,0 +1,16 @@
+import { defineStore } from 'pinia'
+
+export const useTodoListStore = defineStore('todoList', {
+  state: () => ({
+    todoList: [],
+    id: 0
+  }),
+  actions: {
+    addTodo(item) {
+      this.todoList.push({ item, id: this.id++, completed: false })
+    },
+    deleteTodo(itemId) {
+      this.todoList = this.todoList.filter((obj) => obj.id !== itemId)
+    }
+  }
+})
